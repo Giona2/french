4570266782680650563b8c89a13356adf7753bba
Giona2 <153511987+Giona2@users.noreply.github.com>
2023-12-11 18:59:23 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,9 @@ mv ~/french ~/Documents/Python
 mkidr ~/Documents/Python/french/venv
 #set up venv
 python3 -m venv ~/Documents/Python/french/venv
-source ~/Documents/Python/french/venv/bin/activate
+source "~/Documents/Python/french/venv/bin/activate"
 python3 -m pip install --upgrade pip
 #install needed packages
-source ~/Documents/Python/french/venv/bin/activate
 pip install pydub
 pip install SpeechRecognition
 pip install sounddevice
