69525949d2a4866d2d9a537483a9b9778fa46872
Giona2 <153511987+Giona2@users.noreply.github.com>
2023-12-11 18:51:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ python3 -m venv ~/Documents/Python/french/venv
 source ~/Documents/Python/french/venv/bin/activate
 python3 -m pip install --upgrade pip
 #install needed packages
+source ~/Documents/Python/french/venv/bin/activate
 pip install pydub
 pip install SpeechRecognition
 pip install sounddevice
