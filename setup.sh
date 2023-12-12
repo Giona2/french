7f64594aea0c390543ea7a45c8feb28108e0ce33
Giona2 <153511987+Giona2@users.noreply.github.com>
2023-12-11 19:10:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ sudo apt install python3-venv
 mkdir ~/Documents/Python
 mv ~/french ~/Documents/Python
 mkdir ~/Documents/Python/french/venv
+cd ~/Documents/Python/french/
 #set up venv
-python3 -m venv ~/Documents/Python/french/venv
-sleep 5
-source "~/Documents/Python/french/venv/bin/activate"
+python3 -m venv venv
+source "venv/bin/activate"
 python3 -m pip install --upgrade pip
 #install needed packages
 pip install pydub
